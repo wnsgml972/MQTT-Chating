92ee5d415b44b019b13c36290bf710b71d696de3
junhee <wnsgml972@naver.com>
2018-01-17 10:17:53 +0900

    init

---
+++ hifly/src/main/java/kr/ac/hansung/debuger/Debuger.java
@@ -0,0 +1,17 @@
+package kr.ac.hansung.debuger;
+
+public class Debuger {
+	private static final boolean debuger = true;
+
+	public static void printError(Exception e) {
+		if (debuger) {
+			e.getStackTrace();
+		}
+	}
+
+	public static void log(String className, String value) {
+		if (debuger) {
+			System.out.println(value + "  (" + className + ")");
+		}
+	}
+}
---
+++ hifly/src/main/java/kr/ac/hansung/hifly/App.java
@@ -0,0 +1,11 @@
+package kr.ac.hansung.hifly;
+
+import javax.swing.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
